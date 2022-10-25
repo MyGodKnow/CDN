b5fd688b09d59b69ea8b2222b1ddceb099982040
我鬼知道吖~ <95115184+MyGodKnow@users.noreply.github.com>
2022-10-25 10:20:08 +0800

    Update app.js

--- File/app.js
+++ File/app.js
@@ -1,1 +1,1 @@
-particlesJS.load("particles-js","assets/particles.json",function(){console.log("callback - particles.js config loaded")});
+particlesJS.load("particles-js","https://cdn.jsdelivr.net/gh/MyGodKnow/CDN/File/particlesjs-config.json",function(){console.log("callback - particles.js config loaded")});
